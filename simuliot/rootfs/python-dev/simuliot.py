f3f66ac5fdf64dd72582ef2c375a322b3f019b72
keneth-rodriguez <keneth.rodriguez@hpe.com>
2023-08-31 14:20:27 +0200

    updated db locationb

--- simuliot/rootfs/python-dev/simuliot.py
+++ simuliot/rootfs/python-dev/simuliot.py
@@ -24,9 +24,9 @@ logger.info('Starting Simulated IoT Device')
 
 def connect_db():
     conn = None
-    if (os.path.isfile('./tfg-test.db')):
+    if (os.path.isfile('../tfg-test.db')):
         try:
-            conn = sqlite3.connect('./tfg-test.db')
+            conn = sqlite3.connect('../tfg-test.db')
             logger.info("Connected to SQLite DB")
         except sqlite3.Error as e:
             logger.critical('Failure to open DB. Please reinstall addon: ' + e)
