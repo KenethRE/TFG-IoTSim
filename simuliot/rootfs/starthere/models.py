bcbc2f72599c3069d88b067e15ed0fece5786ef7
KenethRE <kenethlaunies.rodriguez@alu.uclm.es>
2024-02-22 17:24:18 +0100

    added mappings for web icons

--- simuliot/rootfs/starthere/models.py
+++ simuliot/rootfs/starthere/models.py
@@ -8,8 +8,31 @@ class Plano(models.Model):
         app_label = 'starthere'
 
 class DeviceManager(models.Manager):
+    def mapTypeToIcon(Type):
+        match Type:
+            case 'Thermometer':
+                return 'thermomether'
+            case 'Water_Flow':
+                return 'water'
+            case 'Air_Flow':
+                return 'air'
+            case 'US_Sensor':
+                return 'sensor'
+            case 'Switch':
+                return 'switch'
+            case 'Hub':
+                return 'router'
+            case 'Switch_Config':
+                return 'tune'
+            case 'Thermo_Config':
+                return 'nest_thermostat'
+            case 'Thermo_Switch':
+                return 'switch'
+            case 'Volume_Sensor':
+                return 'brand_awareness'
+
     def create_device(self, DeviceID, Type, Description, Manufacturer):
-        device = self.create(DeviceID=DeviceID, Type=Type, Description=Description, Manufacturer=Manufacturer)
+        device = self.create(DeviceID=DeviceID, Type=self.mapTypeToIcon(Type), Description=Description, Manufacturer=Manufacturer)
         return device
 
 class Devices(models.Model):
