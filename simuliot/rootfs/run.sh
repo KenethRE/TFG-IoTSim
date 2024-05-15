9737a4049fe014312e4713fe9761230a83196608
Keneth Rodriguez <kenethlaunies.rodriguez@alu.uclm.es>
2024-05-15 22:11:55 +0000

    working mqtt integration

--- simuliot/rootfs/run.sh
+++ simuliot/rootfs/run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/with-contenv bashio
 
 
-MQTT_BROKER_HOST=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PWD=$(bashio::services mqtt "password")
+MQTT_BROKER_HOST=$(bashio::config 'mqtt_broker')
+MQTT_USER=$(bashio::config "mqtt_username")
+MQTT_PWD=$(bashio::config "mqtt_password")
+MQTT_BROKER_PORT=$(bashio::config 'mqtt_port')
 DJANGO_LOG_LEVEL=DEBUG
 
 export DJANGO_LOG_LEVEL
 export MQTT_BROKER_HOST
 export MQTT_USER
 export MQTT_PWD
+export MQTT_BROKER_PORT
 
 { echo "MQTT_BROKER_HOST: $MQTT_BROKER_HOST"; echo "MQTT_USER: $MQTT_USER"; echo "MQTT_PWD: $MQTT_PWD"; } >> /simuliot.log
 
--- simuliot/rootfs/simuliot_frontend/settings.py
+++ simuliot/rootfs/simuliot_frontend/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-ye_)2&2m=&=k$#-oxam950-186)hn4nkhk#v20)vgu4&a4dpe%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8099', '127.0.0.1:8099']
 
 # Application definition
 
